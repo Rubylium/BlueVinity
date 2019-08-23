9b2d2127ef5a67e930ad5190237dfa3a2c372907
Rubyliss <alexisoko0@gmail.com>
2019-08-23 16:25:56 +0200

    élévateur mécano

---
+++ resources/[scripts]/[Mapping]/mecanoMap/README.md
@@ -0,0 +1,3 @@
+Drag and drop modular_carlift-master in your resources folder, rename without "-master", and add start/ensure modular_carlift in your server.cfg
+
+It's only work in the bennys. To make work the carlift, just stand next to the box beside the carlift, press E to open the menu, and test spawn prop to spawn the carlift. Then you can go up or down via the menu.
---
+++ resources/[scripts]/[Mapping]/mecanoMap/config.lua
@@ -0,0 +1,71 @@
+function DisplayHelpText(str)
+	SetTextComponentFormat("STRING")
+	AddTextComponentString(str)
+	DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+end
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		
+		local pos = GetEntityCoords(GetPlayerPed(-1), false)
+		DrawMarker(20, -219.3204, -1326.43, 30.89041, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 3, 252, 48, 100, false, true, 2, false, nil, nil, false)
+		if (Vdist(-219.3204, -1326.43, 29.89041, pos.x, pos.y, pos.z - 1) < 1.5) then
+			DisplayHelpText('Appuyer sur [E] pour ouvrir le menu')
+			if IsControlJustReleased(1, 51) then -- INPUT_CONTEXT
+				PlaySoundFrontend(-1, "SELECT", "HUD_FRONTEND_DEFAULT_SOUNDSET", true)
+				garage_menu = not garage_menu
+				Main()
+			end
+		else
+			if (prevMenu == nil) then
+				Menu.Switch(nil, "")
+				menuOpen = false
+				if garage_menu then
+					garage_menu = false
+				end
+				currentOption = 1
+			elseif not (prevMenu == nil) then
+				if not Menus[prevMenu].previous == nil then
+					currentOption = 1
+					Menu.Switch(nil, prevMenu)
+				else
+					if Menus[prevMenu].optionCount < currentOption then
+						currentOption = Menus[prevMenu].optionCount
+					end
+					Menu.Switch(Menus[prevMenu].previous, prevMenu)
+				end
+			end
+		end
+		
+		if garage_menu then
+			DisableControlAction(1, 22, true)
+			DisableControlAction(1, 0, true)
+			DisableControlAction(1, 27, true)
+			DisableControlAction(1, 140, true)
+			DisableControlAction(1, 141, true)
+			DisableControlAction(1, 142, true)
+			DisableControlAction(1, 20, true)
+			
+			DisableControlAction(1, 187, true)
+			
+			DisableControlAction(1, 80, true)
+			DisableControlAction(1, 95, true)
+			DisableControlAction(1, 96, true)
+			DisableControlAction(1, 97, true)
+			DisableControlAction(1, 98, true)
+			
+			DisableControlAction(1, 81, true)
+			DisableControlAction(1, 82, true)
+			DisableControlAction(1, 83, true)
+			DisableControlAction(1, 84, true)
+			DisableControlAction(1, 85, true)
+			
+			DisableControlAction(1, 74, true)
+			
+			HideHelpTextThisFrame()
+			SetCinematicButtonActive(false)
+			Menu.DisplayCurMenu()
+		end
+	end
+end)
